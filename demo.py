6d72912d83d5db5bd18246671d1a154d747d9579
sailfish009 <sailfish009@gmail.com>
2020-02-01 19:10:31 +0900

    update

--- demo.py
+++ demo.py
@@ -1,45 +1,31 @@
-from mydl.arg.parser import get_args
-from mydl.custom.impl import get_device, get_config, get_logger, get_checkpointer
-from mydl.custom.impl import mixed_precision, get_arguments, get_data_loder_val
-from mydl.data import make_data_loader
-from mydl.solver import make_lr_scheduler, make_optimizer
-from mydl.modeling.detector import build_detection_model
-from mydl.engine.trainer import do_train
-from mydl.utils.comm import is_main_process
+from mydl.custom.impl import setup, run
+from mydl.modeling import build_model
 
-def main():
+import logging
+logger = logging.getLogger("mydl")
 
-    # load config from file and command-line arguments
-    args = get_args("sample")
-    cfg = get_config(args)
+def main(args):
 
-    # logger
-    logger = get_logger(cfg, "", 0)
+    print('config file: {}'.format(args.config_file))
 
-    # model
-    model = build_detection_model(cfg)
-    device = get_device(cfg)
-    model.to(device)
+    cfg = setup(args)
 
-    # optimizer, scheduler
-    optimizer = make_optimizer(cfg, model)
-    scheduler = make_lr_scheduler(cfg, optimizer)
+    model = build_model(cfg)
+    logger.info("Model:\n{}".format(model))
+    if args.eval_only:
+        DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
+            cfg.MODEL.WEIGHTS, resume=args.resume
+        )
+        return do_test(cfg, model)
 
-    # Initialize mixed-precision training
-    model, optimizer = mixed_precision(cfg.DTYPE, model, optimizer)
+    distributed = comm.get_world_size() > 1
+    if distributed:
+        model = DistributedDataParallel(
+            model, device_ids=[comm.get_local_rank()], broadcast_buffers=False
+        )
 
-    # checkpointer
-    save_to_disk = is_main_process()
-    checkpointer, extra_checkpoint_data\
-        = get_checkpointer(cfg, model, optimizer, scheduler, cfg.OUTPUT_DIR, save_to_disk, logger)
-    arguments = get_arguments(extra_checkpoint_data)
-
-    # data loader
-    loader = make_data_loader(cfg, is_train=True, is_distributed=False, start_iter= arguments["iteration"])
-    loader_val, test_period, checkpoint_period = get_data_loder_val(cfg)
-
-    do_train(cfg, model, loader, loader_val, optimizer, scheduler, checkpointer,
-             device, checkpoint_period, test_period, arguments)
+    do_train(cfg, model)
+    return do_test(cfg, model)
 
 
-if __name__ == '__main__': main()
+if __name__ == '__main__': run(main)
