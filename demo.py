ad521f759b40383f434163f58a1f92c25d69d5fb
sailfish009 <sailfish009@gmail.com>
2020-02-07 16:26:42 +0900

    add maskrcnn and kaggle-HPA

--- demo.py
+++ demo.py
@@ -1,19 +1,9 @@
 from mydl.custom.impl import setup, run
 from mydl.modeling import build_model
-from mydl.solver import build_lr_scheduler, build_optimizer
-from mydl.checkpoint import DetectionCheckpointer, PeriodicCheckpointer
-from mydl.data import (
-    MetadataCatalog,
-    build_detection_test_loader,
-    build_detection_train_loader,
-)
-from mydl.utils.events import (
-    CommonMetricPrinter,
-    EventStorage,
-    JSONWriter,
-    TensorboardXWriter,
-)
-import mydl.utils.comm as comm
+from mydl.custom.protein.loss import make_loss_module
+from mydl.custom.protein.data import make_data_loader
+# from mydl.solver import build_lr_scheduler, build_optimizer
+# from mydl.checkpoint import DetectionCheckpointer, PeriodicCheckpointer
 
 import os
 import logging
@@ -21,92 +11,21 @@ logger = logging.getLogger("mydl")
 
 
 def do_train(cfg, model, resume=False):
-    model.train()
-    optimizer = build_optimizer(cfg, model)
-    scheduler = build_lr_scheduler(cfg, optimizer)
-
-    checkpointer = DetectionCheckpointer(
-        model, cfg.OUTPUT_DIR, optimizer=optimizer, scheduler=scheduler
-    )
-    start_iter = (
-        checkpointer.resume_or_load(cfg.MODEL.WEIGHTS, resume=resume).get("iteration", -1) + 1
-    )
-    max_iter = cfg.SOLVER.MAX_ITER
-
-    periodic_checkpointer = PeriodicCheckpointer(
-        checkpointer, cfg.SOLVER.CHECKPOINT_PERIOD, max_iter=max_iter
-    )
-
-    writers = (
-        [
-            CommonMetricPrinter(max_iter),
-            JSONWriter(os.path.join(cfg.OUTPUT_DIR, "metrics.json")),
-            TensorboardXWriter(cfg.OUTPUT_DIR),
-        ]
-        if comm.is_main_process()
-        else []
-    )
-
-    # compared to "train_net.py", we do not support accurate timing and
-    # precise BN here, because they are not trivial to implement
-    data_loader = build_detection_train_loader(cfg)
-    logger.info("Starting training from iteration {}".format(start_iter))
-    with EventStorage(start_iter) as storage:
-        for data, iteration in zip(data_loader, range(start_iter, max_iter)):
-            iteration = iteration + 1
-            storage.step()
-
-            loss_dict = model(data)
-            losses = sum(loss for loss in loss_dict.values())
-            assert torch.isfinite(losses).all(), loss_dict
-
-            loss_dict_reduced = {k: v.item() for k, v in comm.reduce_dict(loss_dict).items()}
-            losses_reduced = sum(loss for loss in loss_dict_reduced.values())
-            if comm.is_main_process():
-                storage.put_scalars(total_loss=losses_reduced, **loss_dict_reduced)
-
-            optimizer.zero_grad()
-            losses.backward()
-            optimizer.step()
-            storage.put_scalar("lr", optimizer.param_groups[0]["lr"], smoothing_hint=False)
-            scheduler.step()
-
-            if (
-                cfg.TEST.EVAL_PERIOD > 0
-                and iteration % cfg.TEST.EVAL_PERIOD == 0
-                and iteration != max_iter
-            ):
-                do_test(cfg, model)
-                # Compared to "train_net.py", the test results are not dumped to EventStorage
-                comm.synchronize()
-
-            if iteration - start_iter > 5 and (iteration % 20 == 0 or iteration == max_iter):
-                for writer in writers:
-                    writer.write()
-            periodic_checkpointer.step(iteration)
+    loss_fn = make_loss_module(cfg)
 
 
 def main(args):
 
     cfg = setup(args)
-    print('config ASPECT_RATIO_GROUPING: {}'.format(cfg.DATALOADER.ASPECT_RATIO_GROUPING))
 
     model = build_model(cfg)
     logger.info("Model:\n{}".format(model))
-    if args.eval_only:
-        DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
-            cfg.MODEL.WEIGHTS, resume=args.resume
-        )
-        return do_test(cfg, model)
-
-    distributed = comm.get_world_size() > 1
-    if distributed:
-        model = DistributedDataParallel(
-            model, device_ids=[comm.get_local_rank()], broadcast_buffers=False
-        )
 
     do_train(cfg, model)
-    return do_test(cfg, model)
+
+
+    # do_train(cfg, model)
+    # return do_test(cfg, model)
 
 
 if __name__ == '__main__': run(main)
