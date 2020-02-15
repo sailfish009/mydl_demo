8d87e2329a9c908b70fe2e051123ae7f465aaa96
sailfish009 <sailfish009@gmail.com>
2020-02-15 18:41:49 +0900

    update

--- demo.py
+++ demo.py
@@ -2,8 +2,14 @@ from mydl.custom.impl import setup, run
 from mydl.modeling import build_model
 from mydl.custom.protein.loss import make_loss_module
 from mydl.custom.protein.data import make_data_loader
-# from mydl.solver import build_lr_scheduler, build_optimizer
+from mydl.solver import build_lr_scheduler, build_optimizer
 # from mydl.checkpoint import DetectionCheckpointer, PeriodicCheckpointer
+from mydl.checkpoint import DetectionCheckpointer
+from mydl.data import (    
+    MetadataCatalog,    
+    build_detection_test_loader,    
+    build_detection_train_loader,    
+)    
 
 import os
 import logging
@@ -12,6 +18,13 @@ logger = logging.getLogger("mydl")
 
 def do_train(cfg, model, resume=False):
     loss_fn = make_loss_module(cfg)
+    optimizer = build_optimizer(cfg, model)
+    scheduler = build_lr_scheduler(cfg, optimizer)
+    checkpointer = DetectionCheckpointer(
+        model, cfg.OUTPUT_DIR, optimizer=optimizer, scheduler=scheduler
+    )
+    data_loader = build_detection_train_loader(cfg)
+
 
 
 def main(args):
